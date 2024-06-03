2aaa4a500361667e696ebe0b5ab3a930c637eb12
michaelwijanto <michael.wijanto@gmail.com>
2024-06-03 17:01:20 +0700

    test

---
+++ app/_layout.jsx
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+// present in all route (navbar/footer)
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app! Hello World</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+// home page (\route)
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const RootLayout = () => {
+  return (
+    <View>
+      <Text>RootLayout</Text>
+    </View>
+  );
+};
+
+export default RootLayout;
+
+const styles = StyleSheet.create({});
