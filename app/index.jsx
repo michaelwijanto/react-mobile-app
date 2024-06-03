733094376320ebb082e9e8e2988df2ab25777c07
michaelwijanto <michael.wijanto@gmail.com>
2024-06-03 18:03:28 +0700

    implement tailwind and tab bar

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,17 @@
 // home page (\route)
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
-const RootLayout = () => {
+export default function App() {
   return (
-    <View>
-      <Text>RootLayout</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Hello World</Text>
+      <Text className="font-plight">This is index</Text>
+      <StatusBar style="auto" />
+      <Link href="/home" style={{ color: "blue" }}>
+        Go to Home
+      </Link>
     </View>
   );
-};
-
-export default RootLayout;
-
-const styles = StyleSheet.create({});
+}
