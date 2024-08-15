c20bd487aedf1531c7ca82a87726aa17ec0928af
michaelwijanto <michael.wijanto@gmail.com>
2024-08-15 14:13:56 +0700

    global state for user session

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -7,8 +7,10 @@ import CustomButton from "../../components/CustomButton";
 import { Link, router } from "expo-router";
 
 import { createUser } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const SignUp = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext();
   const [form, setForm] = useState({
     username: "",
     email: "",
@@ -26,6 +28,8 @@ const SignUp = () => {
       const result = await createUser(form.email, form.password, form.username);
 
       //set to global state...
+      setUser = result;
+      setIsLoggedIn(true);
 
       router.replace("/home");
     } catch (error) {
--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -16,8 +16,10 @@ import VideoCard from "../../components/VideoCard";
 import useAppwrite from "../../lib/useAppwrite";
 // import { placeHolder, exHolder } from "../../testing";
 import { getAllPosts, getLatestPost } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const Home = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const { data: posts, refetch } = useAppwrite(getAllPosts);
   const { data: latestPosts } = useAppwrite(getLatestPost);
   const [refreshing, setRefreshing] = useState(false);
@@ -40,10 +42,10 @@ const Home = () => {
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back
+                  Welcome Back,
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  User ####
+                  {user?.username}
                 </Text>
               </View>
               <View className="mt-1.5">
